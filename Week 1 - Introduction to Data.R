4137c9ced71be2ea0e7255a4926d33f9fdfc35dd
Rwebb1981 <76513305+Rwebb1981@users.noreply.github.com>
2020-12-26 08:28:40 +0000

    Add files via upload

---
+++ Week 1 - Introduction to Data.R
@@ -0,0 +1,11 @@
+#How to load library
+library(SDSFoundations)
+
+#Code to load 'Bike Data' file
+BikeData <- BikeData
+
+#Code to get mean average of 'distance variable'
+mean(BikeData$distance)
+
+#Code to get table of cycling frequency
+table(BikeData$cyc_freq)
