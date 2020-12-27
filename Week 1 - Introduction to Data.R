2b503de0892afce0908f7b29188d36ae27323a46
Rwebb1981 <76513305+Rwebb1981@users.noreply.github.com>
2020-12-27 12:27:34 +0000

    Update Week 1 - Introduction to Data.R
    Additional changes made to this code to include more ways to descriptively analyse data.

--- Week 1 - Introduction to Data.R
+++ Week 1 - Introduction to Data.R
@@ -10,5 +10,20 @@ mean(BikeData$distance)
 #Code to get table of cycling frequency
 table(BikeData$cyc_freq)
 
-#Code to index within a dataframe (i.e. 2nd row 5th column)
+#Code to pull out specific data from a dataframe (i.e. 2nd row 5th column)
 BikeData[2,5]
+
+#Code to pull out data for a specific column
+BikeData[,5]
+
+#Code for counts of categorical variables
+table(BikeData$gender)
+
+#Code to create a new object containing females
+females <- BikeData[BikeData$gender == 'F',]
+
+#Code to create a new variable based on criteria
+femalespeeds <- BikeData$speed[BikeData$gender == 'F']
+
+#Code to print list of variable names
+names(BikeData)
